9b354fc8750788b21ee71086229ed9311e6197e2
answerear <68071730@qq.com>
2015-08-19 23:52:44 +0800

    1、重新调整工程目录结构和文件位置

---
+++ codes/client/Classes/scenemgr/TGScene.h
@@ -0,0 +1,62 @@
+﻿
+
+#ifndef __VISION_TYPE_H__
+#define __VISION_TYPE_H__
+
+
+#include <string>
+#include <map>
+
+typedef signed char			char_t;
+typedef unsigned char       uchar_t;
+typedef signed short		short_t;
+typedef unsigned short      ushort_t;
+typedef signed int			int_t;
+typedef unsigned int        uint_t;
+typedef signed long			long_t;
+typedef unsigned long       ulong_t;
+
+typedef signed char			int8_t;
+typedef unsigned char		uint8_t;
+typedef signed short		int16_t;
+typedef unsigned short		uint16_t;
+typedef signed int			int32_t;
+typedef unsigned int		uint32_t;
+
+typedef signed long long	int64_t;
+typedef unsigned long long	uint64_t;
+
+
+typedef std::string			VString;
+typedef std::wstring		VWString;
+
+
+
+#if defined V_OS_WIN32	// Win32
+
+// 	#include <windows.h>
+
+	#define VEXPORT_API			__declspec(dllexport)
+	#define VIMPORT_API			__declspec(dllimport)
+
+	#ifndef STDCALL
+		#define STDCALL			__stdcall
+	#endif
+
+	#ifdef _WIN64
+		#define V_OS_X64
+	#else
+		#define V_OS_X86
+	#endif
+
+#else
+
+	#define VEXPORT_API
+	#define VIMPORT_API
+
+	#define STDCALL
+
+#endif
+
+
+#endif	/*__VISION_TYPE_H__*/
---
+++ codes/client/Classes/scenemgr/TGSceneManager.h
@@ -0,0 +1,17 @@
+﻿
+#ifndef __TG_SCENE_MANAGER_H__
+#define __TG_SCENE_MANAGER_H__
+
+
+#include "VSingleton.h"
+#include "CCScene.h"
+
+
+class TGSceneManager : public VSingleton<TGSceneManager>
+{
+
+};
+
+
+#endif	/*__TG_SCENE_MANAGER_H__*/
+
